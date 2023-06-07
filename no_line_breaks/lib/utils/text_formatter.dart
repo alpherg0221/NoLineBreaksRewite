b5796cdab103e47128709163f1a1f1fcaeab4b52
alpherg0221 <78247593+alpherg0221@users.noreply.github.com>
2023-06-08 00:16:15 +0900

    Updated text formatter

--- no_line_breaks/lib/utils/text_formatter.dart
+++ no_line_breaks/lib/utils/text_formatter.dart
@@ -6,17 +6,29 @@ extension TextConverter on String {
   String oneLineBreaks() {
     return replaceAll('. ', '.\n')
         .replaceAll('i.e.\n', 'i.e. ')
-        .replaceAll('e.g.\n', 'e.g. ');
+        .replaceAll('e.g.\n', 'e.g. ')
+        .replaceAll('Fig.\n', 'Fig. ')
+        .replaceAll('Tab.\n', 'Tab. ')
+        .replaceAll('et al.\n', 'et al. ')
+        .replaceAll('et.\n\nal.\n', 'et. al. ')
+        .replaceAll(' vs.\n', 'vs. ')
+        .replaceAll('rf.\n', 'rf. ');
   }
 
   String twoLineBreaks() {
     return replaceAll('. ', '.\n')
         .replaceAll('\n', '\n\n')
         .replaceAll('i.e.\n\n', 'i.e. ')
-        .replaceAll('e.g.\n\n', 'e.g. ');
+        .replaceAll('e.g.\n\n', 'e.g. ')
+        .replaceAll('Fig.\n\n', 'Fig. ')
+        .replaceAll('Tab.\n\n', 'Tab. ')
+        .replaceAll('et al.\n\n', 'et al. ')
+        .replaceAll('et.\n\nal.\n\n', 'et. al. ')
+        .replaceAll(' vs.\n\n', ' vs. ')
+        .replaceAll('rf.\n\n', 'rf. ');
   }
 }
 
 String urlText(String text) {
-  return Uri.encodeComponent(text);
+  return Uri.encodeComponent(text).replaceAll('/', '\\%2F');
 }
